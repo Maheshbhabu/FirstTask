ac8c5be58777f83444ef833aa64b2c309abac730
Maheshbhabu <maheshbhabu09@gmail.com>
2023-05-01 12:36:42 +0530

    vino code

--- src/test/java/org/cts/GitTask.java
+++ src/test/java/org/cts/GitTask.java
@@ -13,5 +13,18 @@ public class GitTask {
 		System.out.println("3");
 
 	}
+	private void method7() {
+		System.out.println("7");
+
+	}
+	private void method8() {
+		System.out.println("8");
+
+	}
+	private void method9() {
+		System.out.println("9");
+
+	}
+	
 
 }
